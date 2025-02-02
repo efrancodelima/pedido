8ec7adebbae7a3e6ad730220f9c692cca12c707b
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-02-02 15:39:05 -0300

    Ajuste no script do sonar

--- s7-sonar-qube.sh
+++ s7-sonar-qube.sh
@@ -189,7 +189,7 @@ echo "Quality gate"
 alert_status=$(get_sonar_metric "alert_status")
 alert_status=$(remove_double_quotes $alert_status)
 
-if [ "$(echo "$alert_status == 0" | bc -l)" -eq 1 ]; then
+if [ "$alert_status" = "OK" ]; then
   echo "- Status: ${alert_status}"
 else
   echo "- Status: ${alert_status}"
