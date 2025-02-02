da82552a32f804a85a63abf5924dd298d1b221fc
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-02-02 14:38:16 -0300

    Testes finalizados para os services de cliente

--- src/test/java/br/com/fiap/soat/service/provider/cliente/BuscarClienteServiceTest.java
+++ src/test/java/br/com/fiap/soat/service/provider/cliente/BuscarClienteServiceTest.java
@@ -1,7 +1,66 @@
 package br.com.fiap.soat.service.provider.cliente;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doReturn;
+
+import br.com.fiap.soat.entity.ClienteJpa;
+import br.com.fiap.soat.exception.NotFoundException;
+import br.com.fiap.soat.exception.messages.NotFoundMessage;
+import br.com.fiap.soat.repository.ClienteRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 class BuscarClienteServiceTest {
-  
+
+  AutoCloseable closeable;
+
+  @Mock
+  ClienteRepository repositoryMock;
+
+  @InjectMocks
+  BuscarClienteService service;
+
+  @BeforeEach
+  void setup() {
+    closeable = MockitoAnnotations.openMocks(this);
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+    closeable.close();
+  }
+
+  @Test
+  void deveBuscarClienteComSucesso() throws Exception {
+
+    // Arrange
+    var cliente = new ClienteJpa(1L, 11122233396L, "Nome do cliente", "email@email.com");
+    doReturn(cliente).when(repositoryMock).findByCpf(anyLong());
+
+    // Act
+    var response = service.execute(11122233396L);
+
+    // Assert
+    assertEquals(cliente, response);
+  }
+
+  @Test
+  void deveLancarExcecaoQuandoNaoEncontrarCliente() {
+
+    // Arrange
+    doReturn(null).when(repositoryMock).findByCpf(anyLong());
+
+    // Act and assert
+    var excecao = assertThrows(NotFoundException.class, () -> {
+      service.execute(11122233396L);
+    });
+
+    assertEquals(NotFoundMessage.CPF_CLIENTE.getMessage(), excecao.getMessage());
+  }
 }
--- src/test/java/br/com/fiap/soat/service/provider/cliente/CadastrarClienteServiceTest.java
+++ src/test/java/br/com/fiap/soat/service/provider/cliente/CadastrarClienteServiceTest.java
@@ -1,7 +1,73 @@
 package br.com.fiap.soat.service.provider.cliente;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doReturn;
+
+import br.com.fiap.soat.dto.controller.request.ClienteDto;
+import br.com.fiap.soat.entity.ClienteJpa;
+import br.com.fiap.soat.exception.BusinessRulesException;
+import br.com.fiap.soat.exception.messages.BusinessRulesMessage;
+import br.com.fiap.soat.repository.ClienteRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 class CadastrarClienteServiceTest {
-  
+
+  AutoCloseable closeable;
+
+  @Mock
+  ClienteRepository repositoryMock;
+
+  @InjectMocks
+  CadastrarClienteService service;
+
+  @BeforeEach
+  void setup() {
+    closeable = MockitoAnnotations.openMocks(this);
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+    closeable.close();
+  }
+
+  @Test
+  void deveCadastrarClienteComSucesso() throws Exception {
+
+    // Arrange
+    var clienteDto = new ClienteDto(11122233396L, "Nome do cliente", "email@email.com");
+    var clienteJpa = new ClienteJpa(1L, 11122233396L, "Nome do cliente", "email@email.com");
+    
+    doReturn(null).when(repositoryMock).findByCpf(anyLong());
+    doReturn(clienteJpa).when(repositoryMock).save(any());
+
+    // Act
+    var response = service.execute(clienteDto);
+
+    // Assert
+    assertEquals(clienteJpa, response);
+  }
+
+  @Test
+  void deveLancarExcecaoQuandoClienteJaEstiverCadastrado() {
+
+    // Arrange
+    var clienteDto = new ClienteDto(11122233396L, "Nome do cliente", "email@email.com");
+    
+    doReturn(true).when(repositoryMock).existsByCpf(anyLong());
+
+    // Act and assert
+    var excecao = assertThrows(BusinessRulesException.class, () -> {
+      service.execute(clienteDto);
+    });
+
+    assertEquals(BusinessRulesMessage.CLIENTE_JA_CADASTRADO.getMessage(), excecao.getMessage());
+  }
 }
